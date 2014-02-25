77c0ce31aadbd730ce1bd1d526fbdf17aa2b5246
henryxian <henryxian@qq.com>
2014-02-25 12:23:11 +0800

    add up navigation

--- src/com/henryxian/SettingsActivity.java
+++ src/com/henryxian/SettingsActivity.java
@@ -1,9 +1,11 @@
 package com.henryxian;
 
-import com.actionbarsherlock.app.SherlockPreferenceActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
+import android.support.v4.app.NavUtils;
+
+import com.actionbarsherlock.app.SherlockPreferenceActivity;
+import com.actionbarsherlock.view.MenuItem;
 
 public class SettingsActivity extends SherlockPreferenceActivity{
 	private static final String TAG = SettingsActivity.class.getSimpleName();
@@ -13,6 +15,18 @@ public class SettingsActivity extends SherlockPreferenceActivity{
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.settings);
+		
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 	}
 	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			NavUtils.navigateUpTo(this, new Intent(this, CalendarActivity.class));
+		}
+		
+		return super.onOptionsItemSelected(item);
+	}
 }
