64f8dffeb2077af6611e2c6a7a33f3466db355f3
henryxian <henryxian@qq.com>
2014-03-03 00:08:23 +0800

    add addEventActivity

---
+++ src/com/henryxian/AddEventActivity.java
@@ -0,0 +1,53 @@
+package com.henryxian;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+
+public class AddEventActivity extends SherlockActivity implements OnClickListener{
+	private static final String TAG = AddEventActivity.class.getSimpleName();
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.add_event);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		Intent intent = getIntent();
+		Bundle bundle = intent.getExtras();
+		String date = bundle.getString("Date");
+		TextView textView = (TextView)findViewById(R.id.text_date);
+		textView.setText(date);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// TODO Auto-generated method stub
+		menu.add(R.string.menu_settings)
+			.setIntent(new Intent(this, SettingsActivity.class))
+			.setIcon(R.drawable.ic_action_settings)
+			.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM | MenuItem.SHOW_AS_ACTION_WITH_TEXT);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		NavUtils.navigateUpTo(this, new Intent(this, CalendarActivity.class));
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
+	}
+}
