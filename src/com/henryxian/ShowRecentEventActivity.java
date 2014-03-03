acf07b8b19cd846ff896e1e73463c059b6f28581
henryxian <henryxian@qq.com>
2014-03-03 22:57:08 +0800

    some enhancement.

--- src/com/henryxian/ShowRecentEventActivity.java
+++ src/com/henryxian/ShowRecentEventActivity.java
@@ -1,12 +1,22 @@
 package com.henryxian;
 
+import java.util.HashMap;
+
 import android.content.AsyncQueryHandler;
 import android.content.ContentResolver;
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v4.widget.SimpleCursorAdapter;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
 
 import com.actionbarsherlock.app.SherlockListActivity;
+import com.actionbarsherlock.view.MenuItem;
 import com.henryxian.EventContract.EventEntry;
 
 public class ShowRecentEventActivity extends SherlockListActivity{
@@ -18,6 +28,9 @@ public class ShowRecentEventActivity extends SherlockListActivity{
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		
 		Cursor cur = managedQuery(EventProvider.CONTENT_URI, null, null, null, null);
 		
 //		myAsyncQueryHandler = new MyAsyncQueryHanler(this.getContentResolver());
@@ -31,12 +44,44 @@ public class ShowRecentEventActivity extends SherlockListActivity{
 //				null, 
 //				null
 //			);
-		String[] dataColumns = new String[]{EventEntry.COLUMN_NAME_DATE};
-		int[] viewIDs = {android.R.id.text1};
+		String[] dataColumns = new String[]{
+				EventEntry.COLUMN_NAME_TITLE, 
+				EventEntry.COLUMN_NAME_DATE,
+				EventEntry.COLUMN_NAME_CONTENT
+				};
+		
+		int[] viewIDs = {
+				R.id.text_show_title,  
+				R.id.text_show_date,
+				R.id.text_show_content
+				};
 		adapter = new SimpleCursorAdapter(this, R.layout.event_item, cur, dataColumns, viewIDs);
+		
 		getListView().setAdapter(adapter);
+		getListView().setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position,
+					long id) {
+				// TODO Auto-generated method stub
+				ListView listView = (ListView)parent;
+				Cursor cursor = (Cursor)listView.getItemAtPosition(position);
+				String title = String.valueOf(cursor.getString(cursor.getColumnIndex(EventEntry.COLUMN_NAME_TITLE)));
+				Toast.makeText(ShowRecentEventActivity.this, title, Toast.LENGTH_SHORT).show();
+			}
+			
+		});
 	}
-	
+		
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		switch(item.getItemId()) {
+		case android.R.id.home:
+			NavUtils.navigateUpTo(this, new Intent(this, CalendarActivity.class));
+		}
+		return true;
+	}
 //	private final class MyAsyncQueryHanler extends AsyncQueryHandler {
 //
 //		public MyAsyncQueryHanler(ContentResolver cr) {
